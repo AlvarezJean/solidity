35be87de073993a1dc4607175dcd6eef38797fe8
jalv <jalvarez@octipas.com>
2021-12-21 16:59:39 +0100

    Setting up rinkeby provider

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+// Connect to rinkeby account through infura using mnemonic password
+const provider = new HDWalletProvider('shiver pull slender veteran vapor gospel combine race tiny advance summer mosquito', 'https://rinkeby.infura.io/v3/ad190564806943c096031a4982c4ec47' );
+
+// Create new web3 instance and pass provider
+const web3 = new Web3(provider);
